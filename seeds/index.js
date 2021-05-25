89597f14803a52f08cd36a51402b8cb328478c3a
Aryan Tayal <kashni@Kashnis-MacBook-Pro.local>
2021-05-25 15:34:27 +0530

    Seed

--- seeds/index.js
+++ seeds/index.js
@@ -5,11 +5,11 @@ const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-console.log(dbUrl);
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+    useFindAndModify:false
 });
 
 const db = mongoose.connection;
