07c757ab2fd0924423ea35718c08b39f2107aafb
AryanAnsh2019 <52231066+AryanAnsh2019@users.noreply.github.com>
2021-05-29 16:12:13 +0530

    Add files via upload

---
+++ routes/campgrounds.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const campgrounds = require('../controllers/campgrounds')
+const { campgroundSchema } = require('../schema.js');
+const multer = require('multer')
+const { storage } = require('../cloudinary');
+const upload = multer({ storage })
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+
+router.get('/new', isLoggedIn, campgrounds.newForm)
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.editForm))
+
+module.exports = router;
